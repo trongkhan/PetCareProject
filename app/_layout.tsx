55e5f958a8f8de77618e5e9910dd629fe5711793
trongkhan <trongkhanghqv@gmail.com>
2026-04-14 14:22:05 +0700

    chore: change sqlite behavior

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { initDB } from '@/models/db/client';
+import '@/models/db/client';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,10 +12,6 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
-  useEffect(() => {
-    initDB();
-  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
